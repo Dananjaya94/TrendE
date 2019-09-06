e66e493841c3631ef4429cc926d4b59c30fcd762
Dananjaya94 <nuwan_windows7@live.com>
2019-09-06 21:07:32 +0530

    Commit
    Initial Commit

---
+++ app/src/main/java/com/example/hasiya_mad_project/AddPost.java
@@ -0,0 +1,16 @@
+package com.example.hasiya_mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hasiya_mad_project/Sellers.java
@@ -0,0 +1,14 @@
+package com.example.hasiya_mad_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sellers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_sellers);
+    }
+}
